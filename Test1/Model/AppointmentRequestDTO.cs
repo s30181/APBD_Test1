72a207650db494bc4fd8ef165b4fe49be30ca2c1
s30181 <s30181@pjwstk.edu.pl>
2025-05-12 18:58:39 +0200

    Add require attribute

--- Test1/Model/AppointmentRequestDTO.cs
+++ Test1/Model/AppointmentRequestDTO.cs
@@ -1,15 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tutorial9.Model;
 
 public class AppointmentRequestDTO
 {
+    [Required]
     public int appointmentId { get; set; }
+    
+    [Required]
     public int patientId { get; set; }
+    
+    [Required]
     public string pwz { get; set; }
+    
+    [Required]
     public List<AppointmentServiceRequestDTO> services { get; set; }
 }
 
 public class AppointmentServiceRequestDTO
 {
+    [Required]
     public string serviceName { get; set; }
+    
+    [Required]
     public decimal serviceFee { get; set; }
 }
--- Test1/Model/AppointmentResponseDTO.cs
+++ Test1/Model/AppointmentResponseDTO.cs
@@ -4,27 +4,39 @@ namespace Tutorial9.Model;
 
 public class AppointmentResponseDTO
 {
+    [Required]
     public DateTime date { get; set; }
+    [Required]
     public PatientDTO patient { get; set; }
+    [Required]
     public DoctorDTO doctor { get; set; }
+    [Required]
     public List<AppointmentServiceDTO> appointmentServices { get; set; }
 }
 
 public class PatientDTO
 {
+    [Required]
     public string firstName { get; set; }
+    [Required]
     public string lastName { get; set; }
+    [Required]
     public DateTime dateOfBirth { get; set; }
 }
 
 public class DoctorDTO
 {
+    [Required]
     public int doctorId { get; set; }
+    [Required]
     public string pwz { get; set; }
 }
 
 public class AppointmentServiceDTO
 {
+    [Required]
     public string name { get; set; }
+    
+    [Required]
     public decimal serviceFree { get; set; }
 }
